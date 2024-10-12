2b73d01dbdf70263a402a1dadc96fe2cb3f7d17f
Davlat Pinemelon <davlatpinemelon@Davlats-MacBook-Pro.local>
2024-10-12 14:51:36 +0500

    Added subscribers csv import

--- app-platform/app/Http/Controllers/SubscriberController.php
+++ app-platform/app/Http/Controllers/SubscriberController.php
@@ -5,6 +5,16 @@ namespace App\Http\Controllers;
 use App\Models\Subscriber;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Storage;
+use League\Csv\Exception;
+use League\Csv\InvalidArgument;
+use League\Csv\Reader;
+use League\Csv\Statement;
+use Illuminate\Support\Facades\DB;
+use League\Csv\SyntaxError;
+use League\Csv\UnavailableStream;
 
 class SubscriberController extends Controller
 {
@@ -16,6 +26,7 @@ class SubscriberController extends Controller
     {
         $user = $request->user();
         $subscribers = $user->subscribers;
+
         return response()->json($subscribers);
     }
 
@@ -79,16 +90,81 @@ class SubscriberController extends Controller
     }
 
     /**
+     * Импорт подписчиков через CSV.
+     *
      * @param Request $request
      * @return JsonResponse
+     * @throws Exception
+     * @throws InvalidArgument
+     * @throws SyntaxError
+     * @throws UnavailableStream
      */
     public function import(Request $request): JsonResponse
     {
-        // Логика импорта подписчиков через CSV
-        // Валидация и обработка CSV файла
-        // Пример заглушки
+        $path = false;
 
-        return response()->json(['message' => 'Subscribers imported successfully']);
+        try {
+            $request->validate([
+                'file' => 'required|file|mimes:csv,txt',
+            ]);
+
+            $file = $request->file('file');
+            $path = $file->store('/imports', ['disk' => 'public']);
+
+            $filePath = public_path('storage/' . $path);
+
+
+            $csv = Reader::createFromPath($filePath, 'r');
+            $csv->setHeaderOffset(0); // Если первая строка содержит заголовки
+
+            $stmt = (new Statement())->limit(1000); // Ограничиваем чтение файла для тестирования
+            $records = $stmt->process($csv);
+
+            $errors = [];
+            $subscribers = [];
+
+            foreach ($records as $record) {
+                $validator = Validator::make($record, [
+                    'email' => 'required|email|unique:subscribers,email',
+                    'name' => 'required|string|max:255',
+                ]);
+
+                if ($validator->fails()) {
+                    $errors[] = $validator->errors();
+                } else {
+                    $subscribers[] = [
+                        'email' => $record['email'],
+                        'name' => $record['name'],
+                        'created_at' => now(),
+                        'updated_at' => now(),
+                        'user_id' => $request->user()->getAuthIdentifier(),
+                    ];
+                }
+            }
+
+            if (!empty($errors)) {
+                Log::error('Validation failed', ['errors' => $errors]);
+                throw new \Exception('Validation failed', 422);
+            }
+
+            DB::beginTransaction();
+
+            DB::table('subscribers')->insert($subscribers);
+            Storage::disk('public')->delete($path);
+
+            DB::commit();
+
+            return response()->json(['message' => 'Subscribers imported successfully'], 201);
+        } catch (\Exception $e) {
+            if ($path) {
+                Storage::disk('public')->delete($path);
+            }
+
+            DB::rollBack();
+            Log::error('Import subscribers error: ' . $e->getMessage());
+
+            throw $e;
+        }
     }
 }
 
